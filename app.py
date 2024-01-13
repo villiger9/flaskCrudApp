7d1f36142fa258d205a82e65a79283ed75aac837
villiger9 <ali.shaker93@gmail.com>
2024-01-13 21:41:29 +0300

    added update and delete functionalities

--- app.py
+++ app.py
@@ -41,6 +41,28 @@ def index():
     return render_template('index.html', tasks=tasks, form=form)
 
 
+@app.route('/delete_task/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/update_task/<int:task_id>', methods=['POST', 'GET'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    form = TaskForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        task.title = form.title.data
+        task.description = form.description.data
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('update_task.html', task=task, form=form)
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
